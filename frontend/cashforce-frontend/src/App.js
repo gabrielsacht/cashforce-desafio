fe625e9c6c71b43fe4af8ae8da330b86e457aa5b
gabrielsacht <gabrielsachtr@gmail.com>
2022-12-28 15:09:59 -0300

    setup inicial do frontend com react

---
+++ frontend/cashforce-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header';
+import Page from './components/page';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Page />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/cashforce-frontend/src/components/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>page</h1>
+      </div>
+    );
+  }
+}
+
+export default Page;
